ce31b8628c55b33964e78c89fb450ed8b6e3444b
Verrycken <github@delisvhld.com>
2020-07-18 14:25:08 +0100

    Added axi packages from VUnit, copy. Updated project variables and run.py (broken). RTL for read and write correct by construction.

--- hdlsrc/motorctrl_a4988/rtl/motorctrl_a4988.rtl.vhd
+++ hdlsrc/motorctrl_a4988/rtl/motorctrl_a4988.rtl.vhd
@@ -37,13 +37,14 @@ architecture rtl of motorctrl_a4988 is
   constant c_addr_status : natural := 1;
 begin
   -- Motor driver IC process for A4988 board
-  -- The process only updates the data registers when the slave is addressed
-  -- and only if the address matches a register.
   a4988_proc: process(clk, rst_n)
     variable toggle:    std_logic;
     variable ReadBusy:  boolean;
-    -- Registered version of read address
-    variable ReadAddr: natural range 0 to 2**C_AD_WIDTH-1;
+    variable WriteBusy: boolean;
+    variable WrespBusy: boolean;
+    -- Registered version of addresses
+    variable ReadAddr:  natural range 0 to 2**C_AD_WIDTH-1;
+    variable WriteAddr: natural range 0 to 2**C_AD_WIDTH-1;
     -- Registers
     variable RegStatus: std_logic_vector(G_D_WIDTH-1 downto 0);
     variable RegConfig: std_logic_vector(G_D_WIDTH-1 downto 0);
@@ -53,7 +54,10 @@ begin
       dir       <= '0';
       toggle    := '0';
       ReadBusy  := false;
+      WriteBusy := false;
+      WrespBusy := false;
       ReadAddr  := 0;
+      WriteAddr := 0;
       axils_s2m <= axil_s2m_init;
     elsif rising_edge(clk) then
       step      <= toggle;
@@ -72,16 +76,16 @@ begin
         -- Slave has received address, slave asserts read data valid
         axils_s2m.r.valid       <= '1';
         ReadBusy                := false;
-          -- 32-bit addressing: drop lower two bits (byte address)
-          case (ReadAddr) is
-            when c_addr_config =>
-              axils_s2m.r.data      <= RegConfig;
-            when c_addr_status =>
-              axils_s2m.r.data      <= RegStatus;
-            when others =>
-              -- Undefined register, error response
-              axils_s2m.b.resp      <= axi_response_decerr;
-          end case;
+        axils_s2m.r.resp        <= axi_response_ok;
+        case (ReadAddr) is
+          when c_addr_config =>
+            axils_s2m.r.data    <= RegConfig;
+          when c_addr_status =>
+            axils_s2m.r.data    <= RegStatus;
+          when others =>
+            -- Undefined register, error response
+            axils_s2m.r.resp    <= axi_response_decerr;
+        end case;
       end if;
       -- ===============================
       -- == READ: ADDRESS PHASE       ==
@@ -93,19 +97,70 @@ begin
           -- Slave: address ready + capture address
           axils_s2m.ar.ready    <= '1';
           ReadBusy              := false;
-          axils_s2m.r.resp      <= axi_response_ok;
           -- 32-bit addressing: drop lower two bits (byte address)
+          ReadAddr              :=
+              to_integer(unsigned(axils_m2s.ar.addr(C_AD_WIDTH+1 downto 2)));
           case (ReadAddr) is
             when c_addr_config|c_addr_status =>
               ReadBusy          := true;
-              ReadAddr          :=
-                to_integer(unsigned(axils_m2s.ar.addr(C_AD_WIDTH+1 downto 2)));
             when others =>
               null;
           end case;
         end if;
       end if;
-
+      -- ===============================
+      -- == WRITE: RESPONSE PHASE     ==
+      -- ===============================
+      -- The master must acknowledge the write response is seen
+      if WrespBusy then
+        if axils_m2s.b.ready = '1' then
+          -- Slave has received address, slave asserts 
+          axils_s2m.b.valid     <= '0';
+          WrespBusy             := false;
+        end if;
+      end if;
+      -- ===============================
+      -- == WRITE: DATA PHASE         ==
+      -- ===============================
+      if WriteBusy then
+        if axils_m2s.w.valid = '1' then
+          -- Slave has received address, slave asserts 
+          axils_s2m.w.ready     <= '1';
+          axils_s2m.b.valid     <= '1';
+          WriteBusy             := false;
+          WrespBusy             := true;
+          axils_s2m.b.resp      <= axi_response_ok;
+          case (WriteAddr) is
+            when c_addr_config =>
+              RegConfig         := axils_m2s.w.data;
+            when c_addr_status =>
+              RegStatus         := axils_m2s.w.data;
+            when others =>
+              axils_s2m.b.resp  <= axi_response_decerr;
+          end case;
+        end if;
+      end if;
+      -- ===============================
+      -- == WRITE: ADDRESS PHASE      ==
+      -- ===============================
+      -- If the slave is selected for read access
+      if (axils_m2s_wsel) then
+        -- Master: ar_valid (address valid), r_ready (ready to accept data)
+        if (axils_m2s.aw.valid = '1') then
+          -- Slave: address ready + capture address
+          axils_s2m.aw.ready    <= '1';
+          WriteBusy             := false;
+          -- 32-bit addressing: drop lower two bits (byte address)
+          case (ReadAddr) is
+            when c_addr_config|c_addr_status =>
+              WriteBusy         := true;
+              WriteAddr         :=
+                to_integer(unsigned(axils_m2s.aw.addr(C_AD_WIDTH+1 downto 2)));
+            when others =>
+              null;
+          end case;
+        end if;
+      end if;
     end if;
   end process;
 
--- run.py
+++ run.py
@@ -19,7 +19,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
+import sys
+sys.path.append("")
 from pathlib import Path
 from vunit   import VUnit
 import os
